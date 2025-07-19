fdc3a5b1d1eac25b2ed4d054659139a87f964872
kateemillerhd <kateemillerhd@gmail.com>
2025-07-19 19:51:30 -0400

    Initial commit: Add Substring Detector script

---
+++ substring_detector.rb
@@ -0,0 +1,21 @@
+def substrings(string, dictionary)
+	matches = Hash.new(0)
+	lower_string = string.downcase
+	clean_string = lower_string.gsub(/[^\w\s]/, '').split
+
+	clean_string.each do |word|
+		dictionary.each do |dict_word|
+			if word.include?(dict_word)
+				matches[dict_word] += 1
+			end
+		end
+	end
+
+	matches
+end
+
+dictionary = [
+	"the", "quick", "brown", "fox", "jump", "jumped", "over",
+	"sleep", "sleeping", "dog", "o", "own", "moon"
+]
+
